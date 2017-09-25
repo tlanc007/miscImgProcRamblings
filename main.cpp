6da5cc7a64182029890273d67040634ca203f847
tal <tal.lancaster+gh@gmail.com>
2017-09-25 17:46:33 -0600

    CMakeLists.txt, main.cpp: added timer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
+#include <fmt/printf.h>
+
 #include "image.hpp"
 #include "bmpFormat.hpp"
 
+#define DO_TIMER 1
+
+#if DO_TIMER
+#include "StopWatch.hpp"
+
+void run_timer(const BMPFormat& bmp)
+{
+    //    constexpr auto Loops {100000};
+    constexpr auto Loops {1000};
+
+    {
+        fmt::print ("Timing against {} loops.\n", Loops);
+        StopWatch<> sw ("orig");
+        for (auto i {0u}; i < Loops; ++i) {
+            Image image {bmp};
+            image.rotate(180);
+        }
+    }
+}
+
+#endif
+
 int main ()
 {
-#if 1
+#if 0
     Image image {3, 3, 128};
     image.buildDummyImage();
     image.dumpImage();
@@ -16,6 +40,10 @@ int main ()
     image.copyToBmp(bmp);
 
     bmp.writeFile ("/Volumes/users/tal/Documents/GoogleDrive/Docs/Programming/imageProc/filters/lout_512.bmp");
+
+#  if DO_TIMER
+    run_timer (bmp);
+#  endif
 
 #endif
 }
